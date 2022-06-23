a1516f93b5c99e76b8751ccd749b8cefde4e59e7
Michel <106674127+MichelZero@users.noreply.github.com>
2022-06-23 20:22:49 -0300

    Create ex01.py

---
+++ ex01.py
@@ -0,0 +1,16 @@
+#
+# 1.	Escreva um programa em linguagem Python que receba do usuário três números reais
+# e em seguida imprima-os em tela na ordem inversa.
+# Por exemplo, se o usuário informar os números 15.55, 50.3 e -50.2,
+# o programa deve imprimir em tela estes mesmos números na seguinte sequência: -50.2, 50.3 e 15.55.
+#
+#entrada de dados
+valor1 = float(input("informe o valor 1: ")) # recebe o valor 1
+valor2 = float(input("informe o valor 2: "))
+valor3 = float(input("informe o valor 3: "))
+
+# saida de dados
+print(f"entrada: {valor1}, {valor2}, {valor3}")  # imprime os valores de entrada
+print(f"saida: {valor3}, {valor2}, {valor1}") # imprime os valores de saida
+
+
