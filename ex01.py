ad99581d60ca49ecc13138de19a3bf9a5c4a4177
Carlos Eduardo ✔ <97265556+SmokeDevL@users.noreply.github.com>
2022-06-28 01:02:00 -0300

    Valores Invertidos 🔄

--- ex01.py
+++ ex01.py
@@ -4,7 +4,7 @@
 # Emanuel Franklyn
 # Carlos Eduardo
 #
-# data: 26/06/2022
+# data: 27/06/2022
 #
 # 1. Escreva um programa em linguagem Python que receba do   
 # usuário três números reais e em seguida imprima-os em tela 
@@ -14,11 +14,13 @@
 # 50.3 e 15.55.                                              
 
 # Entrada de dados
+
 valor1 = float(input("informe o valor 1: ")) # Solicita o primeiro valor
 valor2 = float(input("informe o valor 2: ")) # Solicita o segundo valor
 valor3 = float(input("informe o valor 3: ")) # Solicita o terceiro valor
 
 # Saída de dados
+
 print(f"entrada: {valor1}, {valor2}, {valor3}")  # imprime os valores de entrada
 print(f"saída: {valor3}, {valor2}, {valor1}") # imprime os valores de saída
 print("fim do programa") # informa ao usuário que o programa terminou
