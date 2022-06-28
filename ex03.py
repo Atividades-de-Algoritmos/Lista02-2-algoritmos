bedf518a3b7b1a748c5e8bec6a469a91b76b77c9
Carlos Eduardo ✔ <97265556+SmokeDevL@users.noreply.github.com>
2022-06-28 01:07:43 -0300

    Conversão de tempo 🕒

--- ex03.py
+++ ex03.py
@@ -4,7 +4,7 @@
 # Emanuel Franklyn
 # Carlos Eduardo
 #
-# data: 26/06/2022
+# data: 27/06/2022
 #
 # 3. Desenvolva um programa em linguagem Python que após o   
 # usuário informar uma certa quantidade de horas, minutos e  
@@ -12,13 +12,16 @@
 # apresente-o em tela.                                       
 
 # Entrada de dados
-horas = int(input("informe a quantidade de horas: ")) # recebe a quantidade de horas
-minutos = int(input("informe a quantidade de minutos: ")) # recebe a quantidade de minutos
-segundos = int(input("informe a quantidade de segundos: ")) # recebe a quantidade de segundos
 
-# Processamento
-total = (horas * 3600) + (minutos * 60) + segundos # calcula o total de segundos
+horas = int(input("Informe a quantidade de horas: ")) # Solicita a quantidade de horas
+minutos = int(input("Informe a quantidade de minutos: ")) # Solicita a quantidade de minutos
+segundos = int(input("Informe a quantidade de segundos: ")) # Solicita a quantidade de segundos
+
+# Processamento de dados
+
+total = (horas * 3600) + (minutos * 60) + segundos # Calcula o total de segundos
 
 # Saída de dados
-print(f"\ntotal de segundos: {total}s") # imprime o total de segundos
-print("fim do programa") # informa ao usuário que o programa terminou
+
+print(f"\nTotal de segundos: {total}s") # Imprime o total de segundos
+print("fim do programa") # Informa ao usuário que o programa terminou
